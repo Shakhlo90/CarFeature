0f9df91637b576585c34d1102d7d77d1313017e3
Shakhlo <110711642+Shakhlo90@users.noreply.github.com>
2022-12-19 21:15:29 -0500

    the codes are added successfully

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-CarFeature car= new CarFeature();
+CarFeature XLR= new CarFeature();
         System.out.println("The launch the car");
-
-
+        XLR.color="red";
+        XLR.doors="5";
+        XLR.engine=32323;
+        XLR.model="a5";
     }
 }
